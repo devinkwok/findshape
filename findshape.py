495b24d93b8e4fa2db6942848516946facc051e4
devinkwok <devin.kwok@gmail.com>
2026-01-21 01:36:04 -0500

    REFACTOR move transform related math to Shape object

--- findshape.py
+++ findshape.py
@@ -36,6 +36,58 @@ def str2bool(arg):
     return str_arg != "false" and str_arg != "0"
 
 
+class Shape:
+    def __init__(self, object):
+        self.object = object
+        self.path = object.get_path()
+        self.render_transform = object.composed_transform()
+
+        points = [self.render_transform.apply_to_point(vec2d) for vec2d in self.path.end_points]
+        self.points = np.array([[vec2d.x, vec2d.y] for vec2d in points]).T
+        self.centroid = np.mean(self.points, axis=1, keepdims=True)
+
+    @staticmethod
+    def make_transform(size=1.0, matrix=None, translate=None):
+        if matrix is not None:
+            a, b, c, d = matrix[0, 0], matrix[1, 0], matrix[0, 1], matrix[1, 1]
+        else:
+            a, b, c, d = 1, 0, 0, 1
+        t_x = translate[0, 0] if translate is not None else 0
+        t_y = translate[1, 0] if translate is not None else 0
+        transform = Transform((a * size, b * size, c * size, d * size, t_x * size, t_y * size))
+        logging.debug(f'transform {size} {matrix} {translate}, {transform}')
+        return transform
+
+    def get_id(self):
+        return self.object.get_id()
+
+    def center(self, return_inverse=False):
+        self.points -= self.centroid
+        if return_inverse:
+            return self.make_transform(translate=self.centroid)
+        return self.make_transform(translate=-self.centroid)
+
+    def flip_and_rotate_to(self, shape, return_inverse=False):
+        # orthogonal_procrustes finds R that minimizes || R A - B ||
+        # where A and B are 2xn matrices and R is 2x2
+        u, _, vT = np.linalg.svd(shape.points @ self.points.T)
+        R = u @ vT
+        if not np.all(np.isfinite(R)):
+            logging.info(f'NaN or infinity in transformation: {R}')
+            return self.make_transform()
+        self.points = R @ self.points
+        if return_inverse:
+            return self.make_transform(matrix=R.T)
+        return self.make_transform(matrix=R)
+
+    def is_similar(self, shape, mean_tolerance, max_tolerance):
+        diff = self.points - shape.points
+        mean_err = np.mean(diff**2)**0.5
+        max_err = np.max(np.abs(diff))
+        logging.debug(f'mean_err {mean_err}, max_err {max_err}' )
+        return mean_err <= mean_tolerance and max_err <= max_tolerance
+
+
 class FindShape(inkex.EffectExtension):
     def add_arguments(self, parser):
         parser.add_argument("--findrotate", type=str2bool, default=True)
@@ -61,24 +113,26 @@ class FindShape(inkex.EffectExtension):
             raise ValueError("Must select one object as template.")
 
         self.template = elements[0]
-        logging.debug(f'template {self.template.tostring()}')
         try:
-            self.template_nodes, template_translate, template_transform = self.center_path(self.template)
+            logging.debug(f'template {elements[0].tostring()}')
+            self.shape = Shape(self.template)
         except Exception as e:
-            logging.error(f'could not get path from selection: {e}\n{self.template}')
+            logging.error(f'could not get path from selection: {e}\n{self.shape}')
             raise ValueError("Selected object must be a path.")
-        # cache this transform: move template to visible locations, then subtract mean to center around origin
+        # cache this transform: move template to rendered location, then center it
         # note: transforms are applied right to left
-        self.template_transform =  (-template_translate) @ template_transform
+        translate_to_center = self.shape.center()
+        self.template_transform = translate_to_center @ self.shape.render_transform
 
         # look for other objects in file that match template
         matches = []
         for child in self.svg.descendants():
-            if child != self.template:
-                logging.debug(f'comparing... {child.get_id()} {type(child)}')
-                transform = self.match_object(child)
-                if transform is not None:
-                    matches.append((child, transform))
+            if child == self.template:
+                continue  # don't match template to itself
+            logging.debug(f'comparing... {child.get_id()} {type(child)}')
+            transform = self.match_object(child)
+            if transform is not None:
+                matches.append((child, transform))
 
         if len(matches) == 0:
             raise ValueError("No matches found")
@@ -102,7 +156,7 @@ class FindShape(inkex.EffectExtension):
                 logging.debug(f'copy {child.get_id()} to {container.get_id()}')
 
                 if self.options.replacetype == "clone":
-                    copy = self.make_clone(container, transform)
+                    copy = self.copy(container, transform)
                 elif self.options.replacetype == "duplicate":
                     raise NotImplementedError()
 
@@ -114,74 +168,47 @@ class FindShape(inkex.EffectExtension):
             else:
                 self.svg.selection.add(child.get_id())
 
-    @staticmethod
-    def center_path(object):
-        """Returns standardized representation of an object as a path centered about the origin,
-        and the transformations needed to get there.
-
-        Args:
-            object: any element that implements `get_path` (rectangle, circle, etc.)
-
-        Returns:
-            Tuple[np.ndarray, np.ndarray, Transform]: the points of the path representation,
-                with the object's `composed_transform` applied, and then translated to the origin.
-                Also returns the translation vector and the object's `composed_transform`.
-        """
-        transform = object.composed_transform()
-        logging.debug(f'{transform}')
-        path = object.get_path()
-        nodes = [transform.apply_to_point(vec2d) for vec2d in path.end_points]
-        # form matrix of point vectors, each vector has global xy coordinates of node
-        nodes = np.array([[vec2d.x, vec2d.y] for vec2d in nodes]).T
-        center = np.mean(nodes, axis=1, keepdims=True)
-        points = nodes - center
-        translate = Transform(((1, 0, 0, 1, center[0, 0], center[1, 0])))
-        return points, translate, transform
-
-    @staticmethod
-    def orthogonal_procrustes(A, B) -> np.ndarray:
-        # find R that minimizes || R A - B ||
-        # where A and B are 2xn matrices and R is 2x2
-        u, _, vT = np.linalg.svd(B @ A.T)
-        return u @ vT
-
     def match_object(self, object) -> np.ndarray:
         try:
-            # don't need the target transform as it's already applied to the nodes, which is what we will try to match
-            nodes, translate, _ = self.center_path(object)
+            target = Shape(object)
         except Exception as e:
             logging.debug(f'could not get path of object: {e}')
             return None
 
         # check if path has same number of points
-        if nodes.shape != self.template_nodes.shape:
-            logging.debug(f'number of points differs: {nodes.shape}, target is {self.template_nodes.shape}')
+        if target.points.shape != self.shape.points.shape:
+            logging.debug(f'number of points differs: {target.points.shape} and {self.shape.points.shape}')
             return None
+
+        # we want the template -> target transform, so undo centering
+        # note: transforms are applied right to left
+        transform = target.center(return_inverse=True)
 
         # align template to locations of target nodes
-        R = self.orthogonal_procrustes(self.template_nodes, nodes)
-        if not np.all(np.isfinite(R)):
-            logging.debug(f'NaN or infinity in transformation: {R}')
-            return None
+        if self.options.findrescale:
+            raise NotImplementedError  #TODO
+        elif self.options.findresize:
+            raise NotImplementedError  #TODO
+
+        if self.options.findflip and self.options.findrotate:
+            flip_rotate = target.flip_and_rotate_to(self.shape, return_inverse=True)
+            transform = transform @ flip_rotate
+        elif self.options.findflip:
+            raise NotImplementedError  #TODO
+        elif self.options.findrotate:
+            raise NotImplementedError  #TODO
 
         # check error (distance between path points)
-        transformed_template = R @ self.template_nodes
-        mean_error = np.mean((transformed_template - nodes)**2)**0.5
-        if mean_error > self.options.avgerr:
-            logging.debug(f'average error exceeds {self.options.avgerr}: {mean_error}')
-            return None
-        max_error = np.max(np.abs(transformed_template - nodes))
-        if max_error > self.options.maxerr:
-            logging.debug(f'average error exceeds {self.options.maxerr}: {max_error}' )
+        if not target.is_similar(self.shape, self.options.avgerr, self.options.maxerr):
+            logging.debug(f'error exceeds avg {self.options.avgerr} or max {self.options.maxerr}')
             return None
 
-        # compose final transform: transform template to origin, match via procrustes, then undo centering
-        transform = Transform((R[0, 0], R[1, 0], R[0, 1], R[1, 1], 0, 0))
+        # final transform is template -> rendered template -> centered template -> align template to target (flip, rotate, scale, etc.) -> undo centering of target
+        transform = transform @ self.template_transform
         logging.info(f'found match: {transform}')
-        # note: transforms are applied right to left
-        return translate @ transform @ self.template_transform
+        return transform
 
-    def make_clone(self, clone_group, transform: Transform):
+    def copy(self, clone_group, transform: Transform):
         id = self.svg.get_unique_id(f'{self.template.get_id()}-clone')
         # use the Use class from inkex as it nicely formats the transform matrix into a rotation command
         clone = Use.new(self.template, 0, 0, id=id, transform=transform)
